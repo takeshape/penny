eb9e52365499e0e50b60d4b8d9339b8af8aa5966
Michael Shick <m@shick.us>
2022-04-29 14:03:49 -0400

    add lang

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { abortableFetch } from 'abortcontroller-polyfill/dist/cjs-ponyfill';
+import { locale } from 'config';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 // required to use ApolloClient in getStaticProps
 import fetch from 'node-fetch';
@@ -13,7 +14,7 @@ export default class CustomDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang={locale}>
         <Head>
           <link rel="preload" href="/fonts/inter-roman.var.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
           <link
