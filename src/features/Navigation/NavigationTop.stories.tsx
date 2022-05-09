0a705d51a02a560fda0dbcb732c4ad4e08ed4e34
Michael Shick <m@shick.us>
2022-05-09 16:11:11 -0400

    removed unused setup for navigation top

--- src/features/Navigation/NavigationTop.stories.tsx
+++ src/features/Navigation/NavigationTop.stories.tsx
@@ -1,33 +1,10 @@
 import type { ComponentMeta } from '@storybook/react';
-import { GetNavigationDataQuery, SearchStripeProducts } from 'queries';
-import { isSearchOpenAtom } from 'store';
-import GetNavigationDataResult from '../__fixtures__/GetNavigationData.result.json';
-import SearchStripeProductsResults from '../__fixtures__/SearchStripeProducts.result.json';
 import { NavigationTop } from './NavigationTop';
 
 export default {
   title: 'Features/NavigationTop',
   component: NavigationTop,
-  decorators: [(Story) => <div className="relative z-10">{Story()}</div>],
-  parameters: {
-    apolloClient: {
-      mocks: [
-        {
-          request: {
-            query: GetNavigationDataQuery
-          },
-          result: GetNavigationDataResult
-        },
-        {
-          request: {
-            query: SearchStripeProducts,
-            variables: { query: 'socks' }
-          },
-          result: SearchStripeProductsResults
-        }
-      ]
-    }
-  }
+  decorators: [(Story) => <div className="relative z-10">{Story()}</div>]
 } as ComponentMeta<typeof NavigationTop>;
 
 const Template = (args) => <NavigationTop {...args} />;
@@ -37,14 +14,6 @@ export const Mobile = Template.bind({});
 Mobile.parameters = {
   viewport: {
     defaultViewport: 'mobile2'
-  },
-  jotai: {
-    atoms: {
-      isSearchOpen: isSearchOpenAtom
-    },
-    values: {
-      isSearchOpen: false
-    }
   }
 };
 
@@ -53,26 +22,9 @@ export const Tablet = Template.bind({});
 Tablet.parameters = {
   viewport: {
     defaultViewport: 'tablet'
-  },
-  jotai: {
-    atoms: {
-      isSearchOpen: isSearchOpenAtom
-    },
-    values: {
-      isSearchOpen: false
-    }
   }
 };
 
 export const Desktop = Template.bind({});
 
-Desktop.parameters = {
-  jotai: {
-    atoms: {
-      isSearchOpen: isSearchOpenAtom
-    },
-    values: {
-      isSearchOpen: false
-    }
-  }
-};
+Desktop.parameters = {};
