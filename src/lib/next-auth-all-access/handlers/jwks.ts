7f19640cfd77e1b3244b20082db96f314691d83a
Michael Shick <m@shick.us>
2022-05-13 19:26:54 -0400

    cleanup

--- src/lib/next-auth-all-access/handlers/jwks.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import type { HandlerOptions } from '../types';
-
-async function handler(options: HandlerOptions, _req: NextApiRequest, res: NextApiResponse) {
-  res.send(options.jwks);
-}
-
-export default handler;
