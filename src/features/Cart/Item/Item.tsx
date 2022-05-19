e26dcfb0699ae5699d893ae3d0b45b26a6945ac6
Michael Shick <m@shick.us>
2022-05-19 14:05:12 -0400

    Update src/features/Cart/Item/Item.tsx
    Co-authored-by: Allan Lasser <lasser.allan@gmail.com>

--- src/features/Cart/Item/Item.tsx
+++ src/features/Cart/Item/Item.tsx
@@ -59,7 +59,7 @@ export const Item = ({ atom, onRemove }: ItemProps) => {
                   <MinusCircleIcon className="w-5 h-5" aria-hidden="true" />
                 </button>
 
-                <div className="flex items-center w-1 text-gray-500">{quantity}</div>
+                <div className="flex justify-center items-center w-1 text-gray-500">{quantity}</div>
 
                 <a
                   onClick={() => setItem({ ...item, quantity: item.quantity + 1 })}
