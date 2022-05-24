c1cc588bd8c89b3d4ef0b780a9e840446f27c824
Michael Shick <m@shick.us>
2022-05-24 15:23:24 -0400

    clean up

--- src/store.ts
+++ src/store.ts
@@ -12,6 +12,3 @@ export const isMobileMenuOpenAtom = atom(false);
 
 /* Notification */
 export const notificationAtom = atomWithReset<NotificationMessage>({ title: '', body: '' });
-
-/* Loader */
-export const isModalLoaderOpenAtom = atom(false);
