b3d6676ba6b38b651eb866c7dcd22747d9934020
Allan Lasser <lasser.allan@gmail.com>
2022-05-25 13:17:05 -0400

    Fixes broken Storybooks

--- src/features/ProductCategory/ProductCategory.stories.tsx
+++ src/features/ProductCategory/ProductCategory.stories.tsx
@@ -1,5 +1,9 @@
 import type { ComponentMeta, ComponentStory } from '@storybook/react';
+import { _Filters } from './Filters/Filters.stories';
+import { _Header } from './Header/Header.stories';
+import { _Pagination } from './Pagination/Pagination.stories';
 import { ProductCategory } from './ProductCategory';
+import { _ProductGrid } from './ProductGrid/ProductGrid.stories';
 
 const Meta: ComponentMeta<typeof ProductCategory> = {
   title: 'Product Category',
@@ -14,3 +18,9 @@ const Template: ComponentStory<typeof ProductCategory> = (args) => <ProductCateg
 export default Meta;
 
 export const _ProductCategory = Template.bind({});
+_ProductCategory.args = {
+  ..._Header.args,
+  ..._Filters.args,
+  ..._ProductGrid.args,
+  ..._Pagination.args
+};
---
+++ src/features/ProductCategory/ProductGrid/ProductGrid.fixture.ts
@@ -0,0 +1,101 @@
+export const products = [
+  {
+    id: 'gid://shopify/Product/6827068850276',
+    url: '/product/6827068850276',
+    name: 'Elegant Blouse',
+    description: 'A blouse for people on the go.',
+    descriptionHtml: 'A blouse for people on the go.',
+    featuredImage: {
+      height: 1058,
+      url: 'https://cdn.shopify.com/s/files/1/0579/6744/4068/products/blouse.jpg?v=1650570192',
+      width: 820
+    },
+    images: [
+      {
+        height: 1058,
+        url: 'https://cdn.shopify.com/s/files/1/0579/6744/4068/products/blouse.jpg?v=1650570192',
+        width: 820
+      }
+    ],
+    priceMin: {
+      amount: 3000,
+      currencyCode: 'usd'
+    },
+    priceMax: {
+      amount: 3000,
+      currencyCode: 'usd'
+    },
+    variantsCount: 1,
+    reviewsAverage: null,
+    reviewsCount: 0,
+    hasOneTimePurchaseOption: true,
+    hasSubscriptionPurchaseOption: false,
+    data: {}
+  },
+  {
+    id: 'gid://shopify/Product/6827069505636',
+    url: '/product/6827069505636',
+    name: 'Mesh Gym Shorts',
+    description: 'Shorts for anything, anywhere, anytime.',
+    descriptionHtml: 'Shorts for anything, anywhere, anytime.',
+    featuredImage: {
+      height: 791,
+      url: 'https://cdn.shopify.com/s/files/1/0579/6744/4068/products/gymshorts.png?v=1650570316',
+      width: 860
+    },
+    images: [
+      {
+        height: 791,
+        url: 'https://cdn.shopify.com/s/files/1/0579/6744/4068/products/gymshorts.png?v=1650570316',
+        width: 860
+      }
+    ],
+    priceMin: {
+      amount: 2900,
+      currencyCode: 'usd'
+    },
+    priceMax: {
+      amount: 3100,
+      currencyCode: 'usd'
+    },
+    variantsCount: 9,
+    reviewsAverage: 4,
+    reviewsCount: 2,
+    hasOneTimePurchaseOption: true,
+    hasSubscriptionPurchaseOption: true,
+    data: {}
+  },
+  {
+    id: 'gid://shopify/Product/6827068850276',
+    url: '/product/6827068850276',
+    name: 'Elegant Blouse',
+    description: 'A blouse for people on the go.',
+    descriptionHtml: 'A blouse for people on the go.',
+    featuredImage: {
+      height: 1058,
+      url: 'https://cdn.shopify.com/s/files/1/0579/6744/4068/products/blouse.jpg?v=1650570192',
+      width: 820
+    },
+    images: [
+      {
+        height: 1058,
+        url: 'https://cdn.shopify.com/s/files/1/0579/6744/4068/products/blouse.jpg?v=1650570192',
+        width: 820
+      }
+    ],
+    priceMin: {
+      amount: 3000,
+      currencyCode: 'usd'
+    },
+    priceMax: {
+      amount: 3000,
+      currencyCode: 'usd'
+    },
+    variantsCount: 1,
+    reviewsAverage: null,
+    reviewsCount: 0,
+    hasOneTimePurchaseOption: true,
+    hasSubscriptionPurchaseOption: false,
+    data: {}
+  }
+];
