d7ec886839acf4e3d63348344ddb126501bba8a3
Michael Shick <m@shick.us>
2022-05-26 14:58:32 -0400

    better sign in error message

--- src/features/Account/SignIn/SignIn.tsx
+++ src/features/Account/SignIn/SignIn.tsx
@@ -27,6 +27,9 @@ export const AccountSignIn = ({ callbackUrl, error }: AccountSignInProps) => {
     [callbackUrl]
   );
 
+  const hasErrors = Boolean(error);
+  const errorMessage = error === 'Unidentified customer' ? 'Email address or password are incorrect' : error;
+
   return (
     <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
@@ -38,7 +41,7 @@ export const AccountSignIn = ({ callbackUrl, error }: AccountSignInProps) => {
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
           <form action="#" method="POST" className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-            {error && <Alert status="error" primaryText={error} />}
+            {hasErrors && <Alert status="error" primaryText={errorMessage} />}
 
             <Input
               className="sm:col-span-2"
