249042bf96fff6f42fc955440c630596723bc461
Michael Shick <m@shick.us>
2022-06-10 18:06:52 -0400

    link related prods

--- src/features/RelatedProducts/RelatedProducts.tsx
+++ src/features/RelatedProducts/RelatedProducts.tsx
@@ -1,4 +1,5 @@
 import NextImage from 'components/NextImage';
+import NextLink from 'components/NextLink';
 import { quickAddAtom } from 'features/QuickAdd/store';
 import { useSetAtom } from 'jotai';
 import { useCallback } from 'react';
@@ -28,29 +29,31 @@ const RelatedProducts = ({ products }: RelatedProductsProps) => {
       <div className="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
         {products.map((product) => (
           <div key={product.id}>
-            <div className="relative">
-              <div className="relative w-full h-72 rounded-lg overflow-hidden">
-                <NextImage
-                  layout="fill"
-                  src={product.featuredImage.url}
-                  alt={product.featuredImage.altText}
-                  className="w-full h-full object-center object-cover"
-                />
+            <NextLink href={product.url}>
+              <div className="relative">
+                <div className="relative w-full h-72 rounded-lg overflow-hidden">
+                  <NextImage
+                    layout="fill"
+                    src={product.featuredImage.url}
+                    alt={product.featuredImage.altText}
+                    objectFit="cover"
+                  />
+                </div>
+                <div className="relative mt-4">
+                  <h3 className="text-sm font-medium text-gray-900">{product.name}</h3>
+                  {/* <p className="mt-1 text-sm text-gray-500">{product.color}</p> */}
+                </div>
+                <div className="absolute top-0 inset-x-0 h-72 rounded-lg p-4 flex items-end justify-end overflow-hidden">
+                  <div
+                    aria-hidden="true"
+                    className="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"
+                  />
+                  <p className="relative text-lg font-semibold text-white">
+                    {formatPrice(product.priceMin.currencyCode, product.priceMin.amount)}
+                  </p>
+                </div>
               </div>
-              <div className="relative mt-4">
-                <h3 className="text-sm font-medium text-gray-900">{product.name}</h3>
-                {/* <p className="mt-1 text-sm text-gray-500">{product.color}</p> */}
-              </div>
-              <div className="absolute top-0 inset-x-0 h-72 rounded-lg p-4 flex items-end justify-end overflow-hidden">
-                <div
-                  aria-hidden="true"
-                  className="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"
-                />
-                <p className="relative text-lg font-semibold text-white">
-                  {formatPrice(product.priceMin.currencyCode, product.priceMin.amount)}
-                </p>
-              </div>
-            </div>
+            </NextLink>
             <div className="mt-6">
               <a
                 href={product.url}
