c52d7eb793f947f6c492a8cd0ac915a0cb4924ce
Michael Shick <m@shick.us>
2022-06-15 11:53:31 -0400

    stars cmpt

--- src/features/ProductPage/Product/WithImageGrid/ReviewsCallout.tsx
+++ src/features/ProductPage/Product/WithImageGrid/ReviewsCallout.tsx
@@ -1,6 +1,5 @@
-import { StarIcon } from '@heroicons/react/solid';
+import Stars from 'components/Stars/Stars';
 import { ReviewStats } from 'types/review';
-import classNames from 'utils/classNames';
 
 export interface ReviewsCalloutProps {
   stats: ReviewStats;
@@ -12,16 +11,7 @@ export const ReviewsCallout = ({ stats }: ReviewsCalloutProps) => {
       <h3 className="sr-only">Reviews</h3>
       <div className="flex items-center">
         <div className="flex items-center">
-          {[0, 1, 2, 3, 4].map((rating) => (
-            <StarIcon
-              key={rating}
-              className={classNames(
-                stats.average > rating ? 'text-gray-900' : 'text-gray-200',
-                'h-5 w-5 flex-shrink-0'
-              )}
-              aria-hidden="true"
-            />
-          ))}
+          <Stars rating={stats.average} />
         </div>
         <p className="sr-only">{stats.average} out of 5 stars</p>
         <a href="#reviews" className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">
