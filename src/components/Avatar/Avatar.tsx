eca0adbf3746ac6ff80bc569ead9be3e7218edaa
mshick <mshick@users.noreply.github.com>
2022-06-15 17:47:12 +0000

    Lint fix changes

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -13,7 +13,6 @@ export const Avatar = ({ src, alt, size, fallback }: AvatarProps) => {
 
   if (!src) {
     return fallback;
-    
   }
 
   return <NextImage src={src} alt={alt} height={size} width={size} className="rounded-full" />;
