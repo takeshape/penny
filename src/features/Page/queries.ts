08758331a8e014b08c3e7e3cf0b95c321f2d88ab
Michael Shick <m@shick.us>
2022-06-22 16:19:01 -0400

    Page slugs work

---
+++ src/features/Page/queries.ts
@@ -0,0 +1,46 @@
+import { gql } from '@apollo/client';
+import { PagePaginatedList } from 'types/takeshape';
+
+export type PageGetPageSlugsResponse = {
+  pageList: PagePaginatedList;
+};
+
+export const PageGetPageSlugs = gql`
+  query GetPageSlugs {
+    pageList: getPageList(size: 100) {
+      items {
+        _id
+        slug
+      }
+    }
+  }
+`;
+
+export type PageGetPageResponse = {
+  pageList: PagePaginatedList;
+};
+
+export type PageGetPageArgs = {
+  slug: string;
+};
+
+export const PageGetPage = gql`
+  query PageGetPage($slug: String!) {
+    pageList: getPageList(size: 1, where: { slug: { eq: $slug } }) {
+      items {
+        _id
+        sections {
+          ... on PageSectionTitle {
+            heading
+            label
+            subheading
+          }
+          ... on PageSectionMdx {
+            content(format: html)
+          }
+        }
+        title
+      }
+    }
+  }
+`;
