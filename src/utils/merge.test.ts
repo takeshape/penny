1ff64d4f7c974e18963d0911da0467ab859a36dc
Greg Smith <gsmith@incompl.com>
2022-06-22 09:44:18 -0700

    Remove lodash from merge.ts

---
+++ src/utils/merge.test.ts
@@ -0,0 +1,22 @@
+import { mergeDeep } from './merge';
+
+test('mergeDeep', () => {
+  expect(
+    mergeDeep(
+      {
+        string: 'foo',
+        array: ['c', 'd'],
+        object: {
+          string: 'bat'
+        }
+      },
+      {
+        string: 'bar',
+        array: ['a', 'b'],
+        object: {
+          string: 'baz'
+        }
+      }
+    )
+  ).toEqual({ string: 'bar', array: ['a', 'b', 'c', 'd'], object: { string: 'baz' } });
+});
