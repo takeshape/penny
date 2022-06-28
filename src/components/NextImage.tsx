7cc1c28e027ce6df3627cc7ad31f0af28ec4e803
Michael Shick <m@shick.us>
2022-06-28 15:12:33 -0400

    use normal img for storybook

--- src/components/NextImage.tsx
+++ src/components/NextImage.tsx
@@ -3,8 +3,13 @@
 import { isStorybook } from 'config';
 import NextBaseImage, { ImageProps } from 'next/future/image';
 
-const NextImage = (props: ImageProps) => {
-  return <NextBaseImage unoptimized={isStorybook} {...props} />;
+const NextImage = (props: ImageProps & { src: string }) => {
+  if (isStorybook) {
+    // eslint-disable-next-line @next/next/no-img-element
+    return <img alt="" {...props} />;
+  }
+
+  return <NextBaseImage {...props} />;
 };
 
 export default NextImage;
