288449f1d3d73355717fa913e1ec63738e93fffe
Michael Shick <m@shick.us>
2022-06-28 11:40:30 -0400

    remove comment

--- .lighthouse/lighthouseCommentMaker.js
+++ .lighthouse/lighthouseCommentMaker.js
@@ -1,38 +1,9 @@
-// @ts-check
-
-/**
- * @typedef {Object} Summary
- * @prop {number} performance
- * @prop {number} accessibility
- * @prop {number} best-practices
- * @prop {number} seo
- * @prop {number} pwa
- */
-
-/**
- * @typedef {Object} Manifest
- * @prop {string} url
- * @prop {boolean} isRepresentativeRun
- * @prop {string} htmlPath
- * @prop {string} jsonPath
- * @prop {Summary} summary
- */
-
-/**
- * @typedef {Object} LighthouseOutputs
- * @prop {Record<string, string>} links
- * @prop {Manifest[]} manifest
- */
-
 const formatScore = (/** @type { number } */ score) => Math.round(score * 100);
 const emojiScore = (/** @type { number } */ score) => (score >= 0.9 ? '🟢' : score >= 0.5 ? '🟠' : '🔴');
 
 const scoreRow = (/** @type { string } */ label, /** @type { number } */ score) =>
   `| ${emojiScore(score)} ${label} | ${formatScore(score)} |`;
 
-/**
- * @param {LighthouseOutputs} lighthouseOutputs
- */
 function makeComment(lighthouseOutputs) {
   const { summary } = lighthouseOutputs.manifest[0];
   const [[testedUrl, reportUrl]] = Object.entries(lighthouseOutputs.links);
