83f7533d49985c548bd1bdcf065df8102ec29f40
Allan Lasser <lasser.allan@gmail.com>
2022-06-29 15:26:20 -0400

    Adds new navigation to NavigationTop component

--- src/features/Navigation/NavigationTop/components/TopLinks.tsx
+++ src/features/Navigation/NavigationTop/components/TopLinks.tsx
@@ -2,143 +2,96 @@ import { Popover, Transition } from '@headlessui/react';
 import NextLink from 'components/NextLink';
 import { Fragment } from 'react';
 import classNames from 'utils/classNames';
-import { Navigation } from '../../types';
+import { Navigation, NavigationSection } from '../../types';
 
-export const TopLinks = ({ links }: Pick<Navigation, 'links'>) => {
+const SectionWithPopover = ({ section }: { section: NavigationSection }) => (
+  <Popover key={section.name} className="flex">
+    {({ open }) => (
+      <>
+        <div className="relative flex">
+          <Popover.Button
+            className={classNames(
+              open ? 'border-indigo-600 text-indigo-600' : 'border-transparent text-gray-700 hover:text-gray-800',
+              'relative z-10 flex items-center transition-colors ease-out duration-200 text-sm font-medium border-b-2 -mb-px pt-px'
+            )}
+          >
+            {section.name}
+          </Popover.Button>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-200"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition ease-in duration-150"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <Popover.Panel className="absolute top-full inset-x-0 text-gray-500 sm:text-sm">
+            {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
+            <div className="absolute inset-0 top-1/2 bg-white shadow" aria-hidden="true" />
+
+            <div className="relative bg-white">
+              <div className="max-w-7xl mx-auto px-8 pt-10">
+                {section.link && (
+                  <NextLink href={section.link.href} className="hover:text-gray-800">
+                    Shop all {section.name}
+                  </NextLink>
+                )}
+                <div className="grid grid-cols-4 items-start gap-y-10 gap-x-8 pt-10 pb-12">
+                  {section.subsections?.map((subsection, subsectionIdx) => (
+                    <div key={`${subsection.name}-${subsectionIdx}`}>
+                      <p id={`desktop-featured-heading-${subsectionIdx}`} className="font-medium text-gray-900">
+                        {subsection.name}
+                      </p>
+                      <ul
+                        role="list"
+                        aria-labelledby={`desktop-featured-heading-${subsectionIdx}`}
+                        className="mt-6 space-y-6 sm:mt-4 sm:space-y-4"
+                      >
+                        {subsection.links?.map((link) => (
+                          <li key={link.name} className="flex">
+                            <NextLink href={link.href} className="hover:text-gray-800">
+                              {link.name}
+                            </NextLink>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </Popover.Panel>
+        </Transition>
+      </>
+    )}
+  </Popover>
+);
+
+const SectionLink = ({ section }: { section: NavigationSection }) => (
+  <NextLink
+    className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800"
+    href={section.link.href}
+  >
+    {section.name}
+  </NextLink>
+);
+
+export const TopLinks = ({ sections }: Pick<Navigation, 'sections'>) => {
   return (
     <div className="hidden h-full lg:flex">
       {/* Mega menus */}
       <Popover.Group className="ml-8">
         <div className="h-full flex justify-center space-x-8">
-          {links?.categories?.map((category, categoryIdx) => (
-            <Popover key={category.name} className="flex">
-              {({ open }) => (
-                <>
-                  <div className="relative flex">
-                    <Popover.Button
-                      className={classNames(
-                        open
-                          ? 'border-indigo-600 text-indigo-600'
-                          : 'border-transparent text-gray-700 hover:text-gray-800',
-                        'relative z-10 flex items-center transition-colors ease-out duration-200 text-sm font-medium border-b-2 -mb-px pt-px'
-                      )}
-                    >
-                      {category.name}
-                    </Popover.Button>
-                  </div>
-
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-200"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="transition ease-in duration-150"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                  >
-                    <Popover.Panel className="absolute top-full inset-x-0 text-gray-500 sm:text-sm">
-                      {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
-                      <div className="absolute inset-0 top-1/2 bg-white shadow" aria-hidden="true" />
-
-                      <div className="relative bg-white">
-                        <div className="max-w-7xl mx-auto px-8">
-                          <div className="grid grid-cols-2 items-start gap-y-10 gap-x-8 pt-10 pb-12">
-                            <div className="grid grid-cols-2 gap-y-10 gap-x-8">
-                              <div>
-                                <p id={`desktop-featured-heading-${categoryIdx}`} className="font-medium text-gray-900">
-                                  Featured
-                                </p>
-                                <ul
-                                  role="list"
-                                  aria-labelledby={`desktop-featured-heading-${categoryIdx}`}
-                                  className="mt-6 space-y-6 sm:mt-4 sm:space-y-4"
-                                >
-                                  {category.featured?.map((item) => (
-                                    <li key={item.name} className="flex">
-                                      <NextLink href={item.href} className="hover:text-gray-800">
-                                        {item.name}
-                                      </NextLink>
-                                    </li>
-                                  ))}
-                                </ul>
-                              </div>
-                              <div>
-                                <p id="desktop-categories-heading" className="font-medium text-gray-900">
-                                  Categories
-                                </p>
-                                <ul
-                                  role="list"
-                                  aria-labelledby="desktop-categories-heading"
-                                  className="mt-6 space-y-6 sm:mt-4 sm:space-y-4"
-                                >
-                                  {category.categories?.map((item) => (
-                                    <li key={item.name} className="flex">
-                                      <NextLink href={item.href} className="hover:text-gray-800">
-                                        {item.name}
-                                      </NextLink>
-                                    </li>
-                                  ))}
-                                </ul>
-                              </div>
-                            </div>
-                            <div className="grid grid-cols-2 gap-y-10 gap-x-8">
-                              <div>
-                                <p id="desktop-collection-heading" className="font-medium text-gray-900">
-                                  Collection
-                                </p>
-                                <ul
-                                  role="list"
-                                  aria-labelledby="desktop-collection-heading"
-                                  className="mt-6 space-y-6 sm:mt-4 sm:space-y-4"
-                                >
-                                  {category.collection?.map((item) => (
-                                    <li key={item.name} className="flex">
-                                      <NextLink href={item.href} className="hover:text-gray-800">
-                                        {item.name}
-                                      </NextLink>
-                                    </li>
-                                  ))}
-                                </ul>
-                              </div>
-
-                              <div>
-                                <p id="desktop-brand-heading" className="font-medium text-gray-900">
-                                  Brands
-                                </p>
-                                <ul
-                                  role="list"
-                                  aria-labelledby="desktop-brand-heading"
-                                  className="mt-6 space-y-6 sm:mt-4 sm:space-y-4"
-                                >
-                                  {category.brands?.map((item) => (
-                                    <li key={item.name} className="flex">
-                                      <NextLink href={item.href} className="hover:text-gray-800">
-                                        {item.name}
-                                      </NextLink>
-                                    </li>
-                                  ))}
-                                </ul>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </Popover.Panel>
-                  </Transition>
-                </>
-              )}
-            </Popover>
-          ))}
-
-          {links?.pages?.map((page) => (
-            <a
-              key={page.name}
-              href={page.href}
-              className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800"
-            >
-              {page.name}
-            </a>
-          ))}
+          {sections?.map((section) =>
+            section.subsections ? (
+              <SectionWithPopover key={section.name} section={section} />
+            ) : (
+              <SectionLink key={section.name} section={section} />
+            )
+          )}
         </div>
       </Popover.Group>
     </div>
