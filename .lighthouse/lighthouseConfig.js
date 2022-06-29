6d55542b3b723810d573182bdbad3c06edddab92
Michael Shick <m@shick.us>
2022-06-29 17:45:24 -0400

    update emulation profiles

--- .lighthouse/lighthouseConfig.js
+++ .lighthouse/lighthouseConfig.js
@@ -4,49 +4,32 @@ const DEVTOOLS_RTT_ADJUSTMENT_FACTOR = 3.75;
 const DEVTOOLS_THROUGHPUT_ADJUSTMENT_FACTOR = 0.9;
 
 const throttling = {
-  DEVTOOLS_RTT_ADJUSTMENT_FACTOR,
-  DEVTOOLS_THROUGHPUT_ADJUSTMENT_FACTOR,
-  // These values align with WebPageTest's definition of "Fast 3G"
-  // But offer similar charateristics to roughly the 75th percentile of 4G connections.
-  mobileSlow4G: {
-    rttMs: 150,
-    throughputKbps: 1.6 * 1024,
-    requestLatencyMs: 150 * DEVTOOLS_RTT_ADJUSTMENT_FACTOR,
-    downloadThroughputKbps: 1.6 * 1024 * DEVTOOLS_THROUGHPUT_ADJUSTMENT_FACTOR,
-    uploadThroughputKbps: 750 * DEVTOOLS_THROUGHPUT_ADJUSTMENT_FACTOR,
-    cpuSlowdownMultiplier: 4
+  // These values partially align with WebPageTest's definition of "LTE".
+  // https://github.com/WPO-Foundation/webpagetest/blob/master/www/settings/connectivity.ini.sample
+  mobileLTE: {
+    rttMs: 70,
+    throughputKbps: 12 * 1024,
+    requestLatencyMs: 70 * DEVTOOLS_RTT_ADJUSTMENT_FACTOR,
+    downloadThroughputKbps: 12 * 1024 * DEVTOOLS_THROUGHPUT_ADJUSTMENT_FACTOR,
+    uploadThroughputKbps: 12 * 1024 * DEVTOOLS_THROUGHPUT_ADJUSTMENT_FACTOR,
+    cpuSlowdownMultiplier: 2
   },
-  // These values partially align with WebPageTest's definition of "Regular 3G".
-  // These values are meant to roughly align with Chrome UX report's 3G definition which are based
-  // on HTTP RTT of 300-1400ms and downlink throughput of <700kbps.
-  mobileRegular3G: {
-    rttMs: 300,
-    throughputKbps: 700,
-    requestLatencyMs: 300 * DEVTOOLS_RTT_ADJUSTMENT_FACTOR,
-    downloadThroughputKbps: 700 * DEVTOOLS_THROUGHPUT_ADJUSTMENT_FACTOR,
-    uploadThroughputKbps: 700 * DEVTOOLS_THROUGHPUT_ADJUSTMENT_FACTOR,
-    cpuSlowdownMultiplier: 4
-  },
-  // Using a "broadband" connection type
-  // Corresponds to "Dense 4G 25th percentile" in https://docs.google.com/document/d/1Ft1Bnq9-t4jK5egLSOc28IL4TvR-Tt0se_1faTA4KTY/edit#heading=h.bb7nfy2x9e5v
-  desktopDense4G: {
-    rttMs: 40,
-    throughputKbps: 10 * 1024,
+  // These values partially align with WebPageTest's definition of "FIOS".
+  desktopFIOS: {
+    rttMs: 4,
+    throughputKbps: 20 * 1024,
     cpuSlowdownMultiplier: 1,
-    requestLatencyMs: 0, // 0 means unset
-    downloadThroughputKbps: 0,
-    uploadThroughputKbps: 0
+    requestLatencyMs: 4 * DEVTOOLS_RTT_ADJUSTMENT_FACTOR,
+    downloadThroughputKbps: 20 * 1024 * DEVTOOLS_THROUGHPUT_ADJUSTMENT_FACTOR,
+    uploadThroughputKbps: 5 * 1024 * DEVTOOLS_THROUGHPUT_ADJUSTMENT_FACTOR
   }
 };
 
-const MOTOG4_EMULATION_METRICS = {
+const IPHONE_EMULATION_METRICS = {
   mobile: true,
-  width: 360,
-  height: 640,
-  // Moto G4 is really 3, but a higher value here works against
-  // our perf recommendations.
-  // https://github.com/GoogleChrome/lighthouse/issues/10741#issuecomment-626903508
-  deviceScaleFactor: 2.625,
+  width: 390,
+  height: 844,
+  deviceScaleFactor: 2,
   disabled: false
 };
 
@@ -59,7 +42,7 @@ const DESKTOP_EMULATION_METRICS = {
 };
 
 const screenEmulationMetrics = {
-  mobile: MOTOG4_EMULATION_METRICS,
+  mobile: IPHONE_EMULATION_METRICS,
   desktop: DESKTOP_EMULATION_METRICS
 };
 
@@ -77,7 +60,7 @@ module.exports = {
   extends: 'lighthouse:default',
   settings: {
     formFactor: 'desktop',
-    throttling: throttling.desktopDense4G,
+    throttling: throttling.desktopFIOS,
     screenEmulation: screenEmulationMetrics.desktop,
     emulatedUserAgent: userAgents.desktop,
     // audits can be found here:
