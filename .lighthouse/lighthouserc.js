86242f319b4926d34489cb5f6481d934fa25b3f1
Michael Shick <m@shick.us>
2022-06-30 10:08:02 -0400

    reorg

--- .lighthouse/lighthouserc.js
+++ .lighthouse/lighthouserc.js
@@ -1,59 +1,15 @@
-const constants = require('./constants');
+// eslint-disable-next-line no-console
+console.log('LIGHTHOUSE_CONFIG', process.env.LIGHTHOUSE_CONFIG);
 
 module.exports = {
   ci: {
     collect: {
       extends: 'lighthouse:default',
       settings: {
-        formFactor: 'desktop',
-        throttling: constants.throttling.desktopFIOS,
-        screenEmulation: constants.screenEmulationMetrics.desktop,
-        emulatedUserAgent: constants.userAgents.desktop,
-        // audits can be found here:
-        // https://github.com/GoogleChrome/lighthouse/blob/eba2a4d19c5786dc37e993858ff4b663181f81e5/lighthouse-core/config/default-config.js#L174
-        skipAudits: [
-          'canonical', // for preview, this will always be incorrect
-          'is-crawlable', // for preview, always blocked
-          'color-contrast', // doesn't work well with designs
-          'maskable-icon', // not helpful
-          'unsized-images' // not helpful
-        ],
-        onlyCategories: ['accessibility', 'best-practices', 'performance', 'seo', 'pwa'],
-        budgets: [
-          {
-            path: '/*',
-            timings: [
-              {
-                metric: 'interactive',
-                budget: 1000 // 1.0s
-              },
-              {
-                metric: 'first-contentful-paint',
-                budget: 200 // 0.2s
-              },
-              {
-                metric: 'speed-index',
-                budget: 300 // 0.3s
-              }
-            ],
-            resourceSizes: [
-              {
-                resourceType: 'script',
-                budget: 300 // 300k
-              },
-              {
-                resourceType: 'total',
-                budget: 1500 // 1500k
-              }
-            ],
-            resourceCounts: [
-              {
-                resourceType: 'third-party',
-                budget: 10
-              }
-            ]
-          }
-        ]
+        configPath:
+          process.env.LIGHTHOUSE_CONFIG === 'production'
+            ? './.lighthouse/lighthouseConfigProduction.js'
+            : './.lighthouse/lighthouseConfig.js'
       }
     }
   }
