ac10f4f41be34d7cb44c4cae794870922f58b63f
Michael Shick <m@shick.us>
2022-06-30 11:49:10 -0400

    assertions

--- .lighthouse/lighthouserc.js
+++ .lighthouse/lighthouserc.js
@@ -21,7 +21,17 @@ module.exports = {
         'categories:pwa': ['error', { minScore: 0.9 }],
         'color-contrast': 'off',
         'maskable-icon': 'off',
-        'unsized-images': 'off'
+        'unsized-images': 'off',
+        canonical: 'off',
+        'is-crawlable': 'off',
+        'unused-css-rules': 'off',
+        'timing-budget': 'off',
+        'csp-xss': 'off',
+        'unused-javascript': 'off',
+        'uses-responsive-images': 'off',
+        'duplicated-javascript': { maxLength: 2 },
+        'legacy-javascript': { maxLength: 2 },
+        'uses-long-cache-ttl': { maxLength: 5 }
       }
     }
   }
