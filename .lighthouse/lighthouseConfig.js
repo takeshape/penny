0be66bb3f5e500da2234c8d605348273a8242e61
Michael Shick <m@shick.us>
2022-06-30 09:55:55 -0400

    test lighthouse js file

--- .lighthouse/lighthouseConfig.js
+++ .lighthouse/lighthouseConfig.js
@@ -1,68 +1,14 @@
 // see https://github.com/GoogleChrome/lighthouse/blob/master/docs/configuration.md
 
-const DEVTOOLS_RTT_ADJUSTMENT_FACTOR = 3.75;
-const DEVTOOLS_THROUGHPUT_ADJUSTMENT_FACTOR = 0.9;
-
-const throttling = {
-  // These values partially align with WebPageTest's definition of "LTE".
-  // https://github.com/WPO-Foundation/webpagetest/blob/master/www/settings/connectivity.ini.sample
-  mobileLTE: {
-    rttMs: 70,
-    throughputKbps: 12 * 1024,
-    requestLatencyMs: 70 * DEVTOOLS_RTT_ADJUSTMENT_FACTOR,
-    downloadThroughputKbps: 12 * 1024 * DEVTOOLS_THROUGHPUT_ADJUSTMENT_FACTOR,
-    uploadThroughputKbps: 12 * 1024 * DEVTOOLS_THROUGHPUT_ADJUSTMENT_FACTOR,
-    cpuSlowdownMultiplier: 2
-  },
-  // These values partially align with WebPageTest's definition of "FIOS".
-  desktopFIOS: {
-    rttMs: 4,
-    throughputKbps: 20 * 1024,
-    cpuSlowdownMultiplier: 1,
-    requestLatencyMs: 4 * DEVTOOLS_RTT_ADJUSTMENT_FACTOR,
-    downloadThroughputKbps: 20 * 1024 * DEVTOOLS_THROUGHPUT_ADJUSTMENT_FACTOR,
-    uploadThroughputKbps: 5 * 1024 * DEVTOOLS_THROUGHPUT_ADJUSTMENT_FACTOR
-  }
-};
-
-const IPHONE_EMULATION_METRICS = {
-  mobile: true,
-  width: 390,
-  height: 844,
-  deviceScaleFactor: 2,
-  disabled: false
-};
-
-const DESKTOP_EMULATION_METRICS = {
-  mobile: false,
-  width: 1350,
-  height: 940,
-  deviceScaleFactor: 1,
-  disabled: false
-};
-
-const screenEmulationMetrics = {
-  mobile: IPHONE_EMULATION_METRICS,
-  desktop: DESKTOP_EMULATION_METRICS
-};
-
-const MOTOG4_USERAGENT =
-  'Mozilla/5.0 (Linux; Android 7.0; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4420.0 Mobile Safari/537.36 Chrome-Lighthouse';
-const DESKTOP_USERAGENT =
-  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4420.0 Safari/537.36 Chrome-Lighthouse';
-
-const userAgents = {
-  mobile: MOTOG4_USERAGENT,
-  desktop: DESKTOP_USERAGENT
-};
+const constants = require('./constants');
 
 module.exports = {
   extends: 'lighthouse:default',
   settings: {
     formFactor: 'desktop',
-    throttling: throttling.desktopFIOS,
-    screenEmulation: screenEmulationMetrics.desktop,
-    emulatedUserAgent: userAgents.desktop,
+    throttling: constants.throttling.desktopFIOS,
+    screenEmulation: constants.screenEmulationMetrics.desktop,
+    emulatedUserAgent: constants.userAgents.desktop,
     // audits can be found here:
     // https://github.com/GoogleChrome/lighthouse/blob/eba2a4d19c5786dc37e993858ff4b663181f81e5/lighthouse-core/config/default-config.js#L174
     skipAudits: [
