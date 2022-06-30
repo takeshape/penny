95d2bda703057c23de558bff474915355623698b
Michael Shick <m@shick.us>
2022-06-30 11:29:31 -0400

    debug

--- .lighthouse/lighthouserc.js
+++ .lighthouse/lighthouserc.js
@@ -10,6 +10,19 @@ module.exports = {
             ? './.lighthouse/lighthouseConfigProduction.js'
             : './.lighthouse/lighthouseConfigPreview.js'
       }
+    },
+    assert: {
+      preset: 'lighthouse:recommended',
+      assertions: {
+        'categories:performance': ['error', { minScore: 0.9 }],
+        'categories:accessibility': ['error', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 0.9 }],
+        'categories:best-practices': ['error', { minScore: 0.9 }],
+        'categories:pwa': ['error', { minScore: 0.9 }],
+        'color-contrast': 'off',
+        'maskable-icon': 'off',
+        'unsized-images': 'off'
+      }
     }
   }
 };
