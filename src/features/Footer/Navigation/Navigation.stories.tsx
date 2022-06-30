f6c3842e5f9f8ea96fdbb781de4092c81ddf4130
Allan Lasser <lasser.allan@gmail.com>
2022-06-30 10:12:45 -0400

    Fixes storybook

--- src/features/Footer/Navigation/Navigation.stories.tsx
+++ src/features/Footer/Navigation/Navigation.stories.tsx
@@ -1,4 +1,5 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { GetFooterQueryData } from '../Footer.fixtures';
 import { Navigation } from './Navigation';
 
 const Meta: ComponentMeta<typeof Navigation> = {
@@ -10,44 +11,7 @@ const Template: ComponentStory<typeof Navigation> = (args) => <Navigation {...ar
 
 export const _Navigation = Template.bind({});
 _Navigation.args = {
-  sections: [
-    {
-      name: 'Solutions',
-      items: [
-        { name: 'Marketing', href: '#' },
-        { name: 'Analytics', href: '#' },
-        { name: 'Commerce', href: '#' },
-        { name: 'Insights', href: '#' }
-      ]
-    },
-    {
-      name: 'Support',
-      items: [
-        { name: 'Pricing', href: '#' },
-        { name: 'Documentation', href: '#' },
-        { name: 'Guides', href: '#' },
-        { name: 'API Status', href: '#' }
-      ]
-    },
-    {
-      name: 'Company',
-      items: [
-        { name: 'About', href: '#' },
-        { name: 'Blog', href: '#' },
-        { name: 'Jobs', href: '#' },
-        { name: 'Press', href: '#' },
-        { name: 'Partners', href: '#' }
-      ]
-    },
-    {
-      name: 'Legal',
-      items: [
-        { name: 'Claim', href: '#' },
-        { name: 'Privacy', href: '#' },
-        { name: 'Terms', href: '#' }
-      ]
-    }
-  ]
+  sections: GetFooterQueryData.footer.navigation.sections
 };
 
 export default Meta;
