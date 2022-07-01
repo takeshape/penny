7aa07e3f6246e19860aeb21147ad90a23f9ad23e
Michael Shick <m@shick.us>
2022-07-01 17:46:45 -0400

    clean up app.ts

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,2 +1,3 @@
 export const takeshapeAuthIssuer = process.env.NEXT_PUBLIC_TAKESHAPE_AUTH_ISSUER;
 export const takeshapeAuthAudience = process.env.NEXT_PUBLIC_TAKESHAPE_AUTH_AUDIENCE;
+export const sessionRefetchInterval = 30 * 60 // 30 minutes
