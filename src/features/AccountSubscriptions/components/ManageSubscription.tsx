aed75aeae5fad5c99319a8b538bfc896d329cb22
Michael Shick <m@shick.us>
2022-07-26 15:49:45 -0400

    looking good

--- src/features/AccountSubscriptions/components/ManageSubscription.tsx
+++ src/features/AccountSubscriptions/components/ManageSubscription.tsx
@@ -1,91 +1,147 @@
-import { PaperClipIcon } from '@heroicons/react/solid';
-
-// <div>NEXT CHARGE DATE</div>
-// <div>DELIVERY SCHEDULE</div>
-// <div>PRODUCT</div>
-// <div>ORDER NOW</div>
-// <div>SKIP NEXT SHIPMENT</div>
-// <div>SHIPPING ADDRESS</div>
-// <div>PAYMENT METHOD</div>
-// <div>EMAIL ADDRESS</div>
-// <div>CANCEL SUBSCRIPTION</div>
-
+import { getCreditCardIcon } from 'components/Payments/utils';
+import { Subscription } from '../types';
 export interface ManageSubscriptionProps {
-  subscription: any;
+  subscription: Subscription;
 }
 
 export const ManageSubscription = ({ subscription }: ManageSubscriptionProps) => {
+  const product = subscription.products[0];
+  const CreditCardIcon = getCreditCardIcon(subscription.paymentMethod.instrument.brand);
+
   return (
-    <>
-      <div>
-        <h3 className="text-lg leading-6 font-medium text-gray-900">Subscription details</h3>
-        <p className="mt-1 max-w-2xl text-sm text-gray-500">Make changes to your subscription.</p>
+    <div className="p-4 sm:p-6">
+      <div className="flex flex-col lg:grid lg:grid-cols-2 lg:gap-x-6">
+        <div>
+          <h3 className="text-lg leading-6 font-medium text-gray-900">Manage subscription</h3>
+          <p className="mt-1 max-w-2xl text-sm text-gray-500">Make changes to your deliveries, payments and more.</p>
+        </div>
+
+        <div className="flex flex-shrink-0 mt-6 space-x-4 lg:mt-0">
+          <button
+            type="button"
+            className="self-start py-2 px-2.5 border border-transparent rounded-md shadow-sm text-sm font-medium bg-gray-200 text-gray-900 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-full"
+          >
+            Skip Next Delivery
+          </button>
+
+          <button
+            type="button"
+            className="self-start py-2 px-2.5 border border-transparent rounded-md shadow-sm text-sm font-medium bg-gray-200 text-gray-900 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-full"
+          >
+            Order Now
+          </button>
+        </div>
       </div>
-      <div className="mt-5 border-t border-gray-200">
+
+      <div className="mt-6 sm:mt-8 border-t border-gray-200">
         <dl className="divide-y divide-gray-200">
+          {/* Date started */}
+          <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
+            <dt className="text-sm font-medium text-gray-500">Date started</dt>
+            <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              <div className="flex-grow">
+                <time dateTime={subscription.createdDatetime}>{subscription.createdDate}</time>
+              </div>
+            </dd>
+          </div>
+
+          {/* Next charge */}
           <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
             <dt className="text-sm font-medium text-gray-500">Next charge date</dt>
             <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              <span className="flex-grow">{subscription.nextChargeDate}</span>
-              <span className="ml-4 flex-shrink-0">
+              <div className="flex-grow">{subscription.nextChargeDate}</div>
+              <div className="ml-4 flex-shrink-0 flex space-x-4">
                 <button
                   type="button"
                   className="bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   Update
                 </button>
-              </span>
+              </div>
             </dd>
           </div>
+
+          {/* Delivery schedul */}
           <div className="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
             <dt className="text-sm font-medium text-gray-500">Delivery schedule</dt>
             <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              <span className="flex-grow">Backend Developer</span>
-              <span className="ml-4 flex-shrink-0">
+              <div className="flex-grow">{subscription.frequency}</div>
+              <div className="ml-4 flex-shrink-0">
                 <button
                   type="button"
                   className="bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   Update
                 </button>
-              </span>
+              </div>
             </dd>
           </div>
+
+          {/* Product */}
           <div className="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
-            <dt className="text-sm font-medium text-gray-500">Email address</dt>
+            <dt className="text-sm font-medium text-gray-500">Product</dt>
             <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              <span className="flex-grow">margotfoster@example.com</span>
-              <span className="ml-4 flex-shrink-0">
+              <div className="flex-grow">
+                <div>{product.name}</div>
+                <div className="mt-2">{product.variant}</div>
+              </div>
+              <div className="ml-4 flex-shrink-0">
                 <button
                   type="button"
                   className="bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   Update
                 </button>
-              </span>
+              </div>
             </dd>
           </div>
+
+          {/* Quantity */}
           <div className="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
-            <dt className="text-sm font-medium text-gray-500">Salary expectation</dt>
+            <dt className="text-sm font-medium text-gray-500">Quantity</dt>
             <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              <span className="flex-grow"> $120,000</span>
-              <span className="ml-4 flex-shrink-0">
+              <div className="flex-grow">
+                <div className="mt-2">{product.quantity}</div>
+              </div>
+              <div className="ml-4 flex-shrink-0">
                 <button
                   type="button"
                   className="bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   Update
                 </button>
-              </span>
+              </div>
             </dd>
           </div>
+
+          {/* Amount */}
+          <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
+            <dt className="text-sm font-medium text-gray-500">Amount per item</dt>
+            <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              <div className="flex-grow">{product.price}</div>
+            </dd>
+          </div>
+
+          {/* Amount */}
+          <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
+            <dt className="text-sm font-medium text-gray-500">Total Amount</dt>
+            <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              <div className="flex-grow">{subscription.total}</div>
+            </dd>
+          </div>
+
+          {/* Shipping address */}
           <div className="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
-            <dt className="text-sm font-medium text-gray-500">About</dt>
+            <dt className="text-sm font-medium text-gray-500">Shipping address</dt>
             <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
               <span className="flex-grow">
-                Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur
-                qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure
-                nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
+                <span className="block">{subscription.shippingAddress.name}</span>
+                <span className="block mt-1">{subscription.shippingAddress.line1}</span>
+                <span className="block mt-1">{subscription.shippingAddress.line2}</span>
+                <span className="block mt-1">
+                  {subscription.shippingAddress.city}, {subscription.shippingAddress.state}
+                </span>
+                <span className="block mt-1">{subscription.shippingAddress.zip}</span>
               </span>
               <span className="ml-4 flex-shrink-0">
                 <button
@@ -97,61 +153,44 @@ export const ManageSubscription = ({ subscription }: ManageSubscriptionProps) =>
               </span>
             </dd>
           </div>
+
+          {/* Payment method */}
           <div className="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
-            <dt className="text-sm font-medium text-gray-500">Attachments</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              <ul role="list" className="border border-gray-200 rounded-md divide-y divide-gray-200">
-                <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                  <div className="w-0 flex-1 flex items-center">
-                    <PaperClipIcon className="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
-                    <span className="ml-2 flex-1 w-0 truncate">resume_back_end_developer.pdf</span>
-                  </div>
-                  <div className="ml-4 flex-shrink-0 flex space-x-4">
-                    <button
-                      type="button"
-                      className="bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                      Update
-                    </button>
-                    <span className="text-gray-300" aria-hidden="true">
-                      |
-                    </span>
-                    <button
-                      type="button"
-                      className="bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                      Remove
-                    </button>
-                  </div>
-                </li>
-                <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                  <div className="w-0 flex-1 flex items-center">
-                    <PaperClipIcon className="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
-                    <span className="ml-2 flex-1 w-0 truncate">coverletter_back_end_developer.pdf</span>
-                  </div>
-                  <div className="ml-4 flex-shrink-0 flex space-x-4">
-                    <button
-                      type="button"
-                      className="bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                      Update
-                    </button>
-                    <span className="text-gray-300" aria-hidden="true">
-                      |
-                    </span>
-                    <button
-                      type="button"
-                      className="bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                      Remove
-                    </button>
-                  </div>
-                </li>
-              </ul>
+            <dt className="text-sm font-medium text-gray-500">Payment method</dt>
+            <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              <div className="flex-grow">
+                <span className="block">
+                  <CreditCardIcon className="h-6 w-6 inline-block" />
+                  <span className="inline-block ml-2">{subscription.paymentMethod.instrument.brand}</span>{' '}
+                  <span className="inline-block ml-1">{subscription.paymentMethod.instrument.maskedNumber}</span>
+                </span>
+                <span className="block mt-1">
+                  <span className="font-medium text-xs">Expires:</span>{' '}
+                  {subscription.paymentMethod.instrument.expiryMonth}/{subscription.paymentMethod.instrument.expiryYear}
+                </span>
+              </div>
+              <div className="ml-4 flex-shrink-0">
+                <button
+                  type="button"
+                  className="bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Update
+                </button>
+              </div>
             </dd>
+          </div>
+
+          {/* Cancel */}
+          <div className="flex pt-4 pb-2 sm:pt-6 sm:pb-3">
+            <button
+              type="button"
+              className="ml-auto bg-gray-100 py-2 px-2.5 rounded-md shadow-sm text-sm font-medium text-gray-600 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Cancel Subscription
+            </button>
           </div>
         </dl>
       </div>
-    </>
+    </div>
   );
 };
---
+++ src/features/AccountSubscriptions/components/SubscriptionOrders.tsx
@@ -0,0 +1,237 @@
+import { CheckCircleIcon, ClockIcon, InformationCircleIcon, MinusCircleIcon } from '@heroicons/react/solid';
+import NextImage from 'components/NextImage';
+import { compareAsc, format } from 'date-fns';
+import classNames from 'utils/classNames';
+import { Order } from '../types';
+
+const OrderBadge = ({ status, deliveredAt }: Pick<Order, 'status' | 'deliveredAt'>) => {
+  let badgeText = '';
+  let badgeClasses = '';
+
+  switch (status) {
+    case 'delivered':
+      badgeText = `Delivered on ${format(new Date(deliveredAt), 'PPP')}`;
+      badgeClasses = 'bg-green-100 text-green-800';
+      break;
+
+    case 'skipped':
+      badgeText = `Skipped`;
+      badgeClasses = 'bg-gray-100 text-gray-800';
+      break;
+
+    case 'upcoming':
+      badgeText = `Upcoming`;
+      badgeClasses = 'bg-blue-100 text-blue-800';
+      break;
+
+    default:
+      badgeText = `${status}`;
+  }
+
+  return (
+    <span
+      className={classNames(badgeClasses, 'inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium ')}
+    >
+      {badgeText}
+    </span>
+  );
+};
+
+const OrderHeader = ({
+  id,
+  status,
+  fulfillmentDate,
+  deliveredAt
+}: Pick<Order, 'id' | 'status' | 'fulfillmentDate' | 'deliveredAt'>) => {
+  let Icon;
+
+  switch (status) {
+    case 'delivered':
+      const DeliveredIcon = () => (
+        <CheckCircleIcon className="w-5 h-5 text-green-500 inline-block mr-2" aria-hidden="true" />
+      );
+      Icon = DeliveredIcon;
+      break;
+
+    case 'skipped':
+      const SkippedIcon = () => (
+        <MinusCircleIcon className="w-5 h-5 text-gray-500 inline-block mr-2" aria-hidden="true" />
+      );
+      Icon = SkippedIcon;
+      break;
+
+    case 'upcoming':
+      const UpcomingIcon = () => <ClockIcon className="w-5 h-5 text-blue-400 inline-block mr-2" aria-hidden="true" />;
+      Icon = UpcomingIcon;
+      break;
+
+    default:
+      const DefaultIcon = () => (
+        <InformationCircleIcon className="w-5 h-5 text-gray-500 inline-block mr-2" aria-hidden="true" />
+      );
+      Icon = DefaultIcon;
+  }
+
+  return (
+    <>
+      <h2 id={`${id}-heading`} className="text-regular sm:text-lg font-medium text-gray-900 md:flex-shrink-0">
+        <Icon />
+        <time dateTime={fulfillmentDate}>{format(new Date(fulfillmentDate), 'PPP')}</time>
+      </h2>
+      <div className="ml-auto sm:ml-0 space-y-5 md:flex-1 md:min-w-0 sm:flex sm:items-baseline sm:justify-between sm:space-y-0">
+        <p className="text-sm font-medium text-gray-500">
+          <OrderBadge status={status} deliveredAt={deliveredAt} />
+        </p>
+      </div>
+    </>
+  );
+};
+
+const OrderAction = ({ status }: Pick<Order, 'status'>) => {
+  switch (status) {
+    case 'delivered': {
+      return (
+        <button
+          type="button"
+          className="w-full flex items-center justify-center  py-2 px-2.5 border border-transparent rounded-md shadow-sm text-sm font-medium bg-gray-200 text-gray-900 hover:bg-gray-300 hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-full sm:flex-grow-0"
+        >
+          Report an issue
+        </button>
+      );
+    }
+
+    case 'skipped': {
+      return (
+        <button
+          type="button"
+          className="w-full flex items-center justify-center py-2 px-2.5 border border-transparent rounded-md shadow-sm text-sm font-medium bg-gray-200 text-gray-900 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-full sm:flex-grow-0"
+        >
+          Unskip
+        </button>
+      );
+    }
+
+    default: {
+      return (
+        <button
+          type="button"
+          className="w-full flex items-center justify-center py-2 px-2.5 border border-transparent rounded-md shadow-sm text-sm font-medium bg-gray-200 text-gray-900 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-full sm:flex-grow-0"
+        >
+          Skip delivery
+        </button>
+      );
+    }
+  }
+};
+
+const OrderItem = ({ products, ...order }: Order) => {
+  return (
+    <>
+      <div className="space-y-1 flex items-baseline md:space-y-0 sm:space-x-4">
+        <OrderHeader {...order} />
+      </div>
+
+      <div className="mt-2 -mb-6 flow-root border-t border-gray-200 divide-y divide-gray-200">
+        {products.map((product) => (
+          <div key={product.id} className="py-6 sm:flex">
+            <div className="flex space-x-4 sm:min-w-0 sm:flex-1 sm:space-x-6 lg:space-x-8">
+              <NextImage
+                width={100}
+                height={100}
+                src={product.imageSrc}
+                alt={product.imageAlt}
+                className="flex-none w-20 h-20 rounded-md object-center object-cover sm:w-24 sm:h-24"
+              />
+              <div className="pt-1.5 min-w-0 flex-1 sm:pt-0">
+                <h3 className="font-medium text-gray-900">
+                  <a href={product.href}>{product.name}</a>
+                </h3>
+                <p className="text-sm text-gray-500">{product.variant}</p>
+                <p className="text-sm text-gray-500">Quantity: {product.quantity}</p>
+                <p className="mt-1 font-medium text-gray-900">{product.price}</p>
+              </div>
+            </div>
+            <div className="mt-6 space-y-4 sm:mt-0 sm:ml-6 sm:flex-none sm:w-40">
+              <OrderAction status={order.status} />
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export interface SubscriptionOrdersProps {
+  orders: Order[];
+}
+
+export const SubscriptionOrders = ({ orders }: SubscriptionOrdersProps) => {
+  const sortedOrders = orders.sort((a, b) => Date.parse(b.fulfillmentDate) - Date.parse(a.fulfillmentDate));
+  const now = new Date();
+
+  const upcomingOrders = [];
+  const pastOrders = [];
+
+  let previousOrderIsUpcoming = false;
+
+  for (const ord of sortedOrders) {
+    if (compareAsc(new Date(ord.fulfillmentDate), now) === 1) {
+      upcomingOrders.push(ord);
+      previousOrderIsUpcoming = true;
+    } else {
+      if (previousOrderIsUpcoming) {
+        upcomingOrders.push(ord);
+      } else {
+        pastOrders.push(ord);
+      }
+      previousOrderIsUpcoming = false;
+    }
+  }
+
+  return (
+    <div className="p-4 sm:p-6">
+      <div className="flex flex-col lg:grid lg:grid-cols-2 lg:gap-x-6">
+        <div>
+          <h3 className="text-lg leading-6 font-medium text-gray-900">Subscription orders</h3>
+          <p className="mt-1 max-w-2xl text-sm text-gray-500">View past and upcoming orders.</p>
+        </div>
+
+        <div className="flex flex-shrink-0 mt-6 space-x-4 lg:mt-0">
+          <button
+            type="button"
+            className="self-start py-2 px-2.5 border border-transparent rounded-md shadow-sm text-sm font-medium bg-gray-200 text-gray-900 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-full"
+          >
+            Skip Next Delivery
+          </button>
+
+          <button
+            type="button"
+            className="self-start py-2 px-2.5 border border-transparent rounded-md shadow-sm text-sm font-medium bg-gray-200 text-gray-900 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-full"
+          >
+            Order Now
+          </button>
+        </div>
+      </div>
+
+      <div className="mt-6 space-y-16 sm:mt-8">
+        {upcomingOrders.reverse().map((order) => (
+          <section key={order.id} aria-labelledby={`${order.id}-heading`}>
+            <OrderItem {...order} />
+          </section>
+        ))}
+      </div>
+
+      <div className="mt-16">
+        <h3 className="text-lg leading-6 font-medium text-gray-900">Past orders</h3>
+      </div>
+
+      <div className="mt-6 space-y-16 sm:mt-8">
+        {pastOrders.map((order) => (
+          <section key={order.id} aria-labelledby={`${order.id}-heading`}>
+            <OrderItem {...order} />
+          </section>
+        ))}
+      </div>
+    </div>
+  );
+};
