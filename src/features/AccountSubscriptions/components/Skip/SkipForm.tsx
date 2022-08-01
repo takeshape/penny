7b9ec003cb8afbd73e8eb9541ab4355893f5817b
Michael Shick <m@shick.us>
2022-08-01 15:23:51 -0400

    cleaning up

--- src/features/AccountSubscriptions/components/Skip/SkipForm.tsx
+++ src/features/AccountSubscriptions/components/Skip/SkipForm.tsx
@@ -1,9 +1,9 @@
-import { Modal, ModalProps } from 'components/Modal/Modal';
+import { ModalProps } from 'components/Modal/Modal';
 import { format } from 'date-fns';
-import { FormActions } from 'features/AccountSubscriptions/components/FormActions';
-import { useCallback, useEffect, useMemo } from 'react';
+import { ModalForm } from 'features/AccountSubscriptions/components/ModalForm';
+import { ModalFormActions } from 'features/AccountSubscriptions/components/ModalFormActions';
+import { useCallback } from 'react';
 import { useForm } from 'react-hook-form';
-import { shopifyGidToId } from 'transforms/shopify';
 import { Order } from '../../types';
 
 export interface SkipFormProps extends ModalProps {
@@ -29,8 +29,6 @@ export const SkipForm = ({ isOpen, onClose, order }: SkipFormProps) => {
     }
   });
 
-  const orderNumber = useMemo(() => shopifyGidToId(order.id), [order]);
-
   const handleFormSubmit = useCallback(
     async (formData: SkipFormValues) => {
       // eslint-disable-next-line no-console
@@ -43,73 +41,57 @@ export const SkipForm = ({ isOpen, onClose, order }: SkipFormProps) => {
     reset();
   }, [reset]);
 
-  useEffect(() => {
-    let closeTimer;
-
-    if (isSubmitSuccessful && isOpen) {
-      closeTimer = setTimeout(() => onClose(), 2000);
-    }
-
-    return () => {
-      if (closeTimer) {
-        clearTimeout(closeTimer);
-      }
-    };
-  }, [isOpen, isSubmitSuccessful, onClose]);
-
   return (
-    <Modal isOpen={isOpen} onClose={onClose} afterLeave={resetState}>
-      <div className="w-full grid grid-cols-1 gap-y-8 gap-x-6 items-start sm:grid-cols-12 lg:gap-x-8">
-        <div className="sm:col-span-12 lg:col-span-12">
-          <div>
-            <h2 className="text-2xl font-extrabold text-gray-900 sm:pr-12">Skip order</h2>
-            <p className="mt-1 max-w-2xl text-sm text-gray-500">Skip an upcoming order.</p>
+    <ModalForm
+      isOpen={isOpen}
+      onClose={onClose}
+      primaryText="Skip order"
+      secondaryText="Skip an upcoming order."
+      afterLeave={resetState}
+      onSubmit={handleSubmit(handleFormSubmit)}
+      isSubmitSuccessful={isSubmitSuccessful}
+      autoCloseDelay={3000}
+    >
+      <div className="md:h-[calc(1/4*100vh)] overflow-y-scroll p-[1px] flex flex-col">
+        {isSubmitSuccessful ? (
+          <div className="h-full font-medium flex flex-col items-center justify-center text-gray-600">
+            <p className="mb-4">
+              Your order on <strong>{format(new Date(order.fulfillmentDate), 'PPP')}</strong> has been skipped.
+            </p>
+            <p className="text-sm">
+              If this was a mistake you can unskip it on the <strong>Subcription → Orders</strong> screen.
+            </p>
           </div>
+        ) : (
+          <section aria-labelledby="confirm-heading" className="h-full">
+            <h3 id="confirm-heading" className="sr-only">
+              Confirm skip order
+            </h3>
+            {order.status === 'upcoming' && (
+              <div className="h-full font-medium flex flex-col items-center justify-center text-center text-gray-600">
+                <p className="mb-4">
+                  Your order will not be processed on{' '}
+                  <strong className="text-black">{format(new Date(order.fulfillmentDate), 'PPP')}</strong>.
+                </p>
+                <p>Would you like to continue?</p>
+              </div>
+            )}
 
-          <form className="mt-10" onSubmit={handleSubmit(handleFormSubmit)}>
-            <div className="md:h-[calc(1/4*100vh)] overflow-y-scroll p-[1px] flex flex-col">
-              {isSubmitSuccessful ? (
-                <div className="h-full font-medium flex flex-col items-center justify-center text-gray-600">
-                  <p className="mb-2">
-                    Your order on <strong>{format(new Date(order.fulfillmentDate), 'PPP')}</strong> has been skipped.
-                  </p>
-                  <p className="text-sm">
-                    If this was a mistake you can unskip it on the <strong>Subcription → Orders</strong> screen.
-                  </p>
-                </div>
-              ) : (
-                <section aria-labelledby="confirm-heading" className="h-full">
-                  <h3 id="confirm-heading" className="sr-only">
-                    Confirm skip order
-                  </h3>
-                  {order.status === 'upcoming' && (
-                    <div className="h-full font-medium flex flex-col items-center justify-center text-gray-600">
-                      <p className="mb-2">
-                        Your order will not be processed on{' '}
-                        <strong className="text-black">{format(new Date(order.fulfillmentDate), 'PPP')}</strong>.
-                      </p>
-                      <p>Would you like to continue?</p>
-                    </div>
-                  )}
+            {order.status === 'skipped' && <p>This order has already been skipped.</p>}
 
-                  {order.status === 'skipped' && <p>This order has already been skipped.</p>}
-
-                  <input {...register('confirm')} className="hidden" />
-                </section>
-              )}
-            </div>
-
-            <FormActions
-              isSubmitted={isSubmitted}
-              isSubmitting={isSubmitting}
-              onCancel={onClose}
-              className="mt-8 flex justify-end gap-2"
-              submitText="Skip it"
-              disableSubmit={order.status !== 'upcoming'}
-            />
-          </form>
-        </div>
+            <input {...register('confirm')} className="hidden" />
+          </section>
+        )}
       </div>
-    </Modal>
+
+      <ModalFormActions
+        isSubmitted={isSubmitted}
+        isSubmitting={isSubmitting}
+        onCancel={onClose}
+        className="mt-8 flex justify-end gap-2"
+        submitText="Skip it"
+        disableSubmit={order.status !== 'upcoming'}
+      />
+    </ModalForm>
   );
 };
