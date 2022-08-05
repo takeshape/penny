6b043eb473d5f126e3ec4a25cd54f5be8c983bb0
Rob Grant <rob@takeshape.io>
2022-08-05 17:47:54 -0400

    Add Zendesk (#104)

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,53 +1,19 @@
-import { useMutation } from '@apollo/client';
 import { pageRevalidationTtl } from 'config';
-import { Contact, ContactForm } from 'features/Contact/Contact';
-import { CreateTicketMutation } from 'features/Contact/queries';
+import { ContactWithGorgias } from 'features/Contact/ContactWithGorgias';
+import { ContactWithZendesk } from 'features/Contact/ContactWithZendesk';
 import Layout from 'layouts/Default';
 import { getLayoutData } from 'layouts/getLayoutData';
 import { InferGetStaticPropsType, NextPage } from 'next';
-import { useCallback, useState } from 'react';
-import { CreateTicketMutationResponse, CreateTicketMutationVariables } from 'types/takeshape';
+import { useRouter } from 'next/router';
 
 const ContactPage: NextPage = ({ navigation, footer }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const [success, setSuccess] = useState<string>();
-  const [createTicket, { error }] = useMutation<CreateTicketMutationResponse, CreateTicketMutationVariables>(
-    CreateTicketMutation
-  );
+  const router = useRouter();
 
-  const onSubmit = useCallback(
-    async (formValues: ContactForm, recaptchaToken: string) => {
-      const { firstName, lastName, company, email, phoneNumber, message } = formValues;
-
-      const result = await createTicket({
-        variables: {
-          email,
-          message: `From: ${firstName} ${lastName}
-Company: ${company}
-Phone Number: ${phoneNumber}
-${message}`,
-          recaptchaToken
-        }
-      });
-      const { id } = result.data.createTicket;
-      if (id) {
-        setSuccess(`Thank you for reaching out! Created ticket #${id}.`);
-      }
-    },
-    [createTicket]
-  );
+  const Contact = router.query.provider === 'zendesk' ? ContactWithZendesk : ContactWithGorgias;
 
   return (
     <Layout navigation={navigation} footer={footer} seo={{ title: 'Contact' }}>
-      <Contact
-        text={{
-          primary: 'Get in touch',
-          secondary: "We'd be happy to hear from you!",
-          button: 'Send'
-        }}
-        onSubmit={onSubmit}
-        success={success}
-        error={error?.message}
-      />
+      <Contact />
     </Layout>
   );
 };
