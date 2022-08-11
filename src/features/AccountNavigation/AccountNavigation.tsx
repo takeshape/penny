edbda19e391a71070b3b28294d121151c27a5d61
Allan Lasser <lasser.allan@gmail.com>
2022-08-11 11:08:58 -0400

    Mobile account menu (#100)
    * Refactors hardcoded data into AccountNavigation
    
    * Displays account menu on small screens
    
    Cleanup
    
    * Updates design of mobile menu

--- src/features/AccountNavigation/AccountNavigation.tsx
+++ src/features/AccountNavigation/AccountNavigation.tsx
@@ -1,29 +1,58 @@
 import { useApolloClient } from '@apollo/client';
-import { LogoutIcon } from '@heroicons/react/outline';
+import {
+  CreditCardIcon,
+  GiftIcon,
+  KeyIcon,
+  LogoutIcon,
+  RefreshIcon,
+  TagIcon,
+  UserCircleIcon
+} from '@heroicons/react/outline';
 import { signOut } from 'next-auth/react';
 import Link from 'next/link';
-import { useCallback } from 'react';
+import { useRouter } from 'next/router';
+import { useCallback, useMemo } from 'react';
 import classNames from 'utils/classNames';
 
-export interface AccountNavigationProps {
-  items: {
-    name: string;
-    href: string;
-    current: boolean;
-    icon: (props: React.ComponentProps<'svg'>) => JSX.Element;
-  }[];
+export const accountNavigationItems = [
+  { name: 'Account', href: '/account', icon: UserCircleIcon, current: false },
+  { name: 'Password', href: '/account/password', icon: KeyIcon, current: false },
+  { name: 'Purchases', href: '/account/purchases', icon: TagIcon, current: false },
+  { name: 'Subscriptions', href: '/account/subscriptions', icon: RefreshIcon, current: false },
+  { name: 'Payment Methods', href: '/account/payments', icon: CreditCardIcon, current: false },
+  { name: 'Rewards', href: '/account/rewards', icon: GiftIcon, current: false }
+];
+
+function useAccountNavigationItems() {
+  const { asPath } = useRouter();
+  const items = useMemo(
+    () =>
+      accountNavigationItems.map((item) => ({
+        ...item,
+        current: asPath.startsWith(item.href)
+      })),
+    [asPath]
+  );
+  return items;
 }
 
-export const AccountNavigation = ({ items }: AccountNavigationProps) => {
+function useLogout() {
   const { resetStore } = useApolloClient();
-
   const handleLogout = useCallback(async () => {
     await resetStore();
     signOut({ callbackUrl: '/' });
   }, [resetStore]);
+  return {
+    handleLogout
+  };
+}
+
+export const AccountNavigation = () => {
+  const { handleLogout } = useLogout();
+  const items = useAccountNavigationItems();
 
   return (
-    <aside className="py-6 px-2 sm:px-6 lg:py-0 lg:px-0 lg:col-span-3">
+    <aside className="w-full">
       <nav className="space-y-1">
         {items.map((item) => (
           <Link key={item.name} href={item.href}>
@@ -31,7 +60,7 @@ export const AccountNavigation = ({ items }: AccountNavigationProps) => {
               className={classNames(
                 item.current
                   ? 'bg-primary-50 text-accent-700 hover:text-accent-700 hover:bg-background'
-                  : 'text-primary-900 hover:text-primary-900 hover:bg-primary-50',
+                  : 'text-primary-800 hover:text-primary-800 hover:bg-primary-50',
                 'group rounded-md px-3 py-2 flex items-center text-sm font-medium w-full'
               )}
               aria-current={item.current ? 'page' : undefined}
---
+++ src/features/Navigation/NavigationMobileMenu/components/MobileMenuAccount.tsx
@@ -0,0 +1,41 @@
+import { AccountNavigation } from 'features/AccountNavigation/AccountNavigation';
+import { signIn, useSession } from 'next-auth/react';
+
+export const MobileMenuAccount = () => {
+  const { status } = useSession();
+
+  if (status === 'authenticated') {
+    return (
+      <div className="py-6 px-4 space-y-6">
+        <div className="flow-root">
+          <AccountNavigation />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="py-4 px-4">
+      <a
+        href={`/api/auth/signin`}
+        onClick={(e) => {
+          e.preventDefault();
+          signIn();
+        }}
+        className="text-body-800 hover:text-body-800 hover:bg-body-50 m-0 cursor-pointer rounded-md px-3 py-2 flex items-center text-sm font-medium w-full"
+      >
+        Create an account
+      </a>
+      <a
+        href={`/api/auth/signin`}
+        onClick={(e) => {
+          e.preventDefault();
+          signIn();
+        }}
+        className="text-body-800 hover:text-body-800 hover:bg-body-50 m-0 cursor-pointer rounded-md px-3 py-2 flex items-center text-sm font-medium w-full"
+      >
+        Sign in
+      </a>
+    </div>
+  );
+};
