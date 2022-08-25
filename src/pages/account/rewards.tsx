48e3d6c533a08141f3e0de87f0d05350811aca88
Andrew Sprouse <sprouse.a@gmail.com>
2022-08-25 18:17:41 -0400

    wire up LL

--- src/pages/account/rewards.tsx
+++ src/pages/account/rewards.tsx
@@ -4,11 +4,14 @@ import { AccountReferrals } from 'features/AccountReferrals/AccountReferrals';
 import Layout from 'layouts/Account';
 import { getLayoutData } from 'layouts/getLayoutData';
 import { InferGetStaticPropsType, NextPage } from 'next';
-import { GetMyLoyaltyCardQueryResponse } from 'types/takeshape';
+import { GetMyLoyaltyCardQueryResponse, GetMyLoyaltyLionCustomerQueryResponse } from 'types/takeshape';
 import { useAuthenticatedQuery } from 'utils/takeshape';
+import { GetMyLoyaltyLionCustomerQuery } from '../../features/AccountLoyaltyLion/queries';
+import { AccountLoyaltyLion } from '../../features/AccountLoyaltyLion/AccountLoyaltyLion';
 
 const AccountRewardsPage: NextPage = ({ navigation, footer }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const { data } = useAuthenticatedQuery<GetMyLoyaltyCardQueryResponse>(GetMyLoyaltyCardQuery);
+  // const { data } = useAuthenticatedQuery<GetMyLoyaltyCardQueryResponse>(GetMyLoyaltyCardQuery);
+  const { data } = useAuthenticatedQuery<GetMyLoyaltyLionCustomerQueryResponse>(GetMyLoyaltyLionCustomerQuery);
 
   if (!data) {
     return null;
@@ -17,11 +20,7 @@ const AccountRewardsPage: NextPage = ({ navigation, footer }: InferGetStaticProp
   return (
     <Layout navigation={navigation} footer={footer} seo={{ title: 'Rewards' }}>
       <AccountReferrals />
-      <AccountLoyaltyCard
-        code={data?.loyaltyCard.code}
-        loyalty_card={data?.loyaltyCard.loyalty_card}
-        assets={data?.loyaltyCard.assets}
-      />
+      <AccountLoyaltyLion {...data.loyaltyCard} />
     </Layout>
   );
 };
