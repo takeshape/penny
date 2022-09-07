c2dc4498a6a75a92ff4fd48f4ccfef67facc45d7
Michael Shick <m@shick.us>
2022-09-07 17:29:04 -0400

    restore query

--- src/features/AccountSubscriptions/queries.ts
+++ src/features/AccountSubscriptions/queries.ts
@@ -310,18 +310,18 @@ export const SetNextChargeDateMutation = gql`
   }
 `;
 
-// export const CreateOnetimeMutation = gql`
-//   mutation CreateOnetimeMutation($addressId: String!, $productId: String!, $variantId: String!, $quantity: Int!) {
-//     onetime: Recharge_createOnetime(
-//       addressId: $addressId
-//       productId: $productId
-//       variantId: $variantId
-//       quantity: $quantity
-//     ) {
-//       id
-//     }
-//   }
-// `;
+export const CreateOnetimeMutation = gql`
+  mutation CreateOnetimeMutation($addressId: String!, $productId: String!, $variantId: String!, $quantity: Int!) {
+    onetime: Recharge_createOnetime(
+      addressId: $addressId
+      productId: $productId
+      variantId: $variantId
+      quantity: $quantity
+    ) {
+      id
+    }
+  }
+`;
 
 export const UpdateDeliveryFrequencyMutation = gql`
   ${SubscriptionFragment}
