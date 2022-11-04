752e929f849edd498b6b602d646088591e9c8751
Michael Shick <m@shick.us>
2022-11-04 15:46:00 -0400

    increase delay multiplier

--- src/utils/apollo/retryGraphqlThrottle.ts
+++ src/utils/apollo/retryGraphqlThrottle.ts
@@ -1,6 +1,8 @@
 import { ApolloError, ApolloQueryResult } from '@apollo/client';
 import { sleep } from '../sleep';
 
+const delayMultiplier = 20;
+
 // Shopify may throttle in a GraphQL error rather than 429. As a result
 // it bypasses RetryLink. Apollo onError can catch it but isn't suited for
 // throttle handling as it is synchronous and cannot retry more than once.
@@ -13,7 +15,7 @@ export const retryGraphqlThrottle = async <T>(
   } catch (e) {
     if (e instanceof ApolloError && e.message === 'Throttled' && attempt < 10) {
       // Exponential backoff with jitter
-      await sleep(Math.round(2 ** attempt * 10 + Math.random() * 1000));
+      await sleep(Math.round(2 * attempt * delayMultiplier + Math.random() * 1000));
       return await retryGraphqlThrottle(execute, attempt + 1);
     } else {
       throw e;
