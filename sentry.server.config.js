f323b303401e7c79664a1defff8e9672a0c55af0
Michael Shick <m@shick.us>
2022-11-06 11:08:12 -0500

    disable sentry

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -2,12 +2,12 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs';
+// import * as Sentry from '@sentry/nextjs';
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+// const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-Sentry.init({
-  dsn: SENTRY_DSN,
-  tracesSampleRate: 1.0,
-  environment: process.env.VERCEL_ENV ?? 'development'
-});
+// Sentry.init({
+//   dsn: SENTRY_DSN,
+//   tracesSampleRate: 1.0,
+//   environment: process.env.VERCEL_ENV ?? 'development'
+// });
