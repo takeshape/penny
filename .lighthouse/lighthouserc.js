e03cbf11d55515faf74170eecafcbd044390e240
Michael Shick <m@shick.us>
2022-12-01 16:59:24 -0500

    try inlining config

--- .lighthouse/lighthouserc.js
+++ .lighthouse/lighthouserc.js
@@ -1,33 +1,59 @@
 module.exports = {
   ci: {
     collect: {
+      extends: 'lighthouse:default',
       settings: {
-        configPath: './.lighthouse/lighthouseConfig.js'
+        // configPath: './.lighthouse/lighthouseConfig.js'
+        formFactor: 'desktop',
+        throttling: constants.throttling.desktopFIOS,
+        screenEmulation: constants.screenEmulationMetrics.desktop,
+        emulatedUserAgent: constants.userAgents.desktop,
+        // audits can be found here:
+        // https://github.com/GoogleChrome/lighthouse/blob/eba2a4d19c5786dc37e993858ff4b663181f81e5/lighthouse-core/config/default-config.js#L174
+        skipAudits: [
+          'canonical', // even when testing the production build, we still need to use the build url
+          'is-crawlable', // vercel will send a x-robots-txt header to avoid crawling the build (non-canonical) url
+          'color-contrast', // doesn't work well with designs
+          'maskable-icon', // not helpful
+          'unsized-images' // not helpful
+        ],
+        onlyCategories: ['accessibility', 'best-practices', 'performance', 'seo', 'pwa'],
+        budgets: [
+          {
+            path: '/*',
+            timings: [
+              {
+                metric: 'interactive',
+                budget: 1000 // 1.0s
+              },
+              {
+                metric: 'first-contentful-paint',
+                budget: 200 // 0.2s
+              },
+              {
+                metric: 'speed-index',
+                budget: 300 // 0.3s
+              }
+            ],
+            resourceSizes: [
+              {
+                resourceType: 'script',
+                budget: 300 // 300k
+              },
+              {
+                resourceType: 'total',
+                budget: 1500 // 1500k
+              }
+            ],
+            resourceCounts: [
+              {
+                resourceType: 'third-party',
+                budget: 10
+              }
+            ]
+          }
+        ]
       }
     }
-    // TODO Make this work or remove
-    // assert: {
-    //   preset: 'lighthouse:recommended',
-    //   assertions: {
-    //     'categories:performance': ['error', { minScore: 0.9 }],
-    //     'categories:accessibility': ['error', { minScore: 0.9 }],
-    //     'categories:seo': ['error', { minScore: 0.9 }],
-    //     'categories:best-practices': ['error', { minScore: 0.9 }],
-    //     'categories:pwa': ['error', { minScore: 0.9 }],
-    //     'color-contrast': 'off',
-    //     'maskable-icon': 'off',
-    //     'unsized-images': 'off',
-    //     canonical: 'off',
-    //     'is-crawlable': 'off',
-    //     'unused-css-rules': 'off',
-    //     'timing-budget': 'off',
-    //     'csp-xss': 'off',
-    //     'unused-javascript': 'off',
-    //     'uses-responsive-images': 'off',
-    //     'duplicated-javascript': { maxLength: 2 },
-    //     'legacy-javascript': { maxLength: 2 },
-    //     'uses-long-cache-ttl': { maxLength: 5 }
-    //   }
-    // }
   }
 };
