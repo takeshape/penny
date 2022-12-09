d7d8a65fd4111faa789b41048f99e12204698147
Michael Shick <m@shick.us>
2022-12-09 11:12:23 -0500

    Delete boom.ts

--- src/pages/api/boom.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiHandler } from 'next';
-import { withSentry } from 'utils/api/withSentry';
-
-const handler: NextApiHandler = () => {
-  throw new Error('BOOM');
-};
-
-export default withSentry(handler);
