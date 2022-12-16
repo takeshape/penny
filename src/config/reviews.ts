a62d407f5f23f3f53384b52966b389e5a5e72228
Andrew Sprouse <sprouse.a@gmail.com>
2022-12-16 09:17:33 -0500

    Disable Trustpilot by default

--- src/config/reviews.ts
+++ src/config/reviews.ts
@@ -1,4 +1,3 @@
 export const enableReviewsIo = true;
-export const reviewsIoReviewsPerPage = 3;
 export const enableTrustpilot = false;
-export const trustpilotReviewsPerPage = 3;
+export const reviewsPerPage = 3;
