52a1bb915fbe98b65d024a92a8b50460334a40c8
Michael Shick <m@shick.us>
2022-12-21 16:25:49 -0500

    csp again

--- next.config.mjs
+++ next.config.mjs
@@ -15,10 +15,6 @@ const withBundleAnalyzer = createBundleAnalyzer({
 const ContentSecurityPolicy = `
   default-src * 'unsafe-inline' 'unsafe-eval';
   script-src * 'unsafe-inline' 'unsafe-eval';
-  connect-src * 'unsafe-inline';
-  img-src * data: blob: 'unsafe-inline';
-  frame-src *;
-  style-src * 'unsafe-inline';
 `;
 
 const securityHeaders = [
