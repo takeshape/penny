b75216c93a4702dffa372e8f932c4c6196a8163f
Michael Shick <m@shick.us>
2023-01-25 14:52:59 -0500

    Drop image res size

--- next.config.mjs
+++ next.config.mjs
@@ -106,7 +106,7 @@ const nextConfig = {
   images: {
     minimumCacheTTL: 60,
     formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 750, 828, 1024],
+    deviceSizes: [640, 750, 828],
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
     remotePatterns: [
       {
