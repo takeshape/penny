224a0dbba8782347243ddabb282a79ba9c20cb30
Michael Shick <m@shick.us>
2023-01-25 07:18:18 -0500

    suppress err log

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ -n "$VERCEL_GIT_COMMIT_REF" ]] 
+if [[ -n "$VERCEL_GIT_COMMIT_REF" ]]
 then
   npm install --quiet -g @takeshape/cli
 
@@ -8,9 +8,9 @@ then
     echo `takeshape branch tagVersion --tag ${VERCEL_GIT_COMMIT_SHA}` > .current-branch-url
 
   # There is a takeshape branch that matches this git branch
-  elif takeshape branch url --name ${VERCEL_GIT_COMMIT_REF}; then
+  elif takeshape branch url --name ${VERCEL_GIT_COMMIT_REF} &> /dev/null; then
     echo `takeshape branch tagVersion --name ${VERCEL_GIT_COMMIT_REF} --tag ${VERCEL_GIT_COMMIT_SHA}` > .current-branch-url
-  
+
   # No matching takeshape branch
   else
     echo `takeshape branch tagVersion --tag ${VERCEL_GIT_COMMIT_SHA}` > .current-branch-url
