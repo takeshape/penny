141cc9bb2e1c582255a02e39168f5e83938beec7
Michael Shick <m@shick.us>
2023-05-11 10:42:24 -0400

    fix customerId

--- src/utils/account.ts
+++ src/utils/account.ts
@@ -1,5 +1,7 @@
+import { shopifyCustomerIdToGid } from 'transforms/shopify';
+
 export function getActivationParams(type: 'reset' | 'activate', path: string) {
   const relevantPath = path.split(`/${type}/`)[1];
   const [customerId, token] = relevantPath.split('/');
-  return { customerId, token };
+  return { customerId: shopifyCustomerIdToGid(customerId), token };
 }
