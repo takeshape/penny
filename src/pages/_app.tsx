92d5e6c81cc445a1485a79c9a826131433e689a5
Michael Shick <m@shick.us>
2023-06-15 16:02:43 -0400

    Bump dependencies (#184)
    * use next/font
    
    * remove abortController hack

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { SessionProvider } from 'next-auth/react';
 import { ReCaptchaProvider } from 'next-recaptcha-v3';
 import { DefaultSeo } from 'next-seo';
 import { AppContext, AppInitialProps } from 'next/app';
+import { inter } from 'styles/fonts';
 import 'styles/globals.css';
 
 if (sentryDsn) {
@@ -27,7 +28,9 @@ export default function App({ Component, pageProps }: AppContext & AppInitialPro
         >
           <ApolloProvider pageProps={pageProps}>
             <DefaultSeo {...seo} />
-            <Component {...pageProps} />
+            <main className={`${inter.variable} antialiased font-sans`}>
+              <Component {...pageProps} />
+            </main>
           </ApolloProvider>
         </SessionProvider>
       </ReCaptchaProvider>
