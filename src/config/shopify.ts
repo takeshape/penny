deaa66dac84d6198140c03c99b5d2bf9c4a8c54c
Michael Shick <m@shick.us>
2023-06-21 17:52:34 -0400

    Add configurable checkout URL

--- src/config/shopify.ts
+++ src/config/shopify.ts
@@ -4,3 +4,4 @@ export const shopifyStorefrontUrl = assertEnv(process.env.NEXT_PUBLIC_SHOPIFY_ST
 export const shopifyStorefrontToken = assertEnv(process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_TOKEN);
 export const shopifyMultipassSecret = process.env.SHOPIFY_MULTIPASS_SECRET ?? '';
 export const shopifyUseMultipass = process.env.NEXT_PUBLIC_SHOPIFY_USE_MULTIPASS === 'true';
+export const shopifyCheckoutRedirectUrl = '{{origin}}';
--- src/features/Cart/utils.ts
+++ src/features/Cart/utils.ts
@@ -1,15 +1,19 @@
+import { shopifyCheckoutRedirectUrl } from 'config';
 import { CartItem } from 'features/Cart/types';
 import { Session } from 'next-auth';
+import Stach from 'stach';
 import { ProductPriceOption } from 'types/product';
 import { CartCreateMutationVariables, CartLineInput } from 'types/storefront';
 
 export const getCartVariables = (items: CartItem[], session: Session | null, discountCode: string | null) => {
+  const redirectOrigin = Stach(shopifyCheckoutRedirectUrl, { origin: window.location.origin });
+
   const createCartVariables: CartCreateMutationVariables = {
     input: {
       attributes: [
         {
           key: 'redirect_origin',
-          value: window.location.origin
+          value: redirectOrigin
         }
       ],
       lines: items?.map(
