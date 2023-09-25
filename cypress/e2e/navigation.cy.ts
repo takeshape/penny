b39dcbd304b4367c9891b91f93f2fee949992485
Michael Shick <m@shick.us>
2023-09-25 14:23:15 -0400

    remove cypress, host report

--- cypress/e2e/navigation.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('Rendering and navigation', () => {
-  it('should navigate between pages without crashing', () => {
-    cy.visit('/about');
-    cy.findAllByText(/Spindrift/i).should('exist');
-  });
-});
