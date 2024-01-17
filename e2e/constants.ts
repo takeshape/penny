42bd12bbe148f0ee9fcb92d9e25c56b77d671229
PollySt <polina_stadnichenko@pecodesoftware.com>
2024-01-17 15:48:26 +0200

    update constants

--- e2e/constants.ts
+++ e2e/constants.ts
@@ -1,4 +1,4 @@
-export const PRODUCT_NAME = process.env.PLAYWRIGHT_PRODUCT_NAME;
-export const BASE_URL = process.env.PLAYWRIGHT_TEST_BASE_URL;
+export const PRODUCT_NAME = process.env.PLAYWRIGHT_PRODUCT_NAME!;
+export const BASE_URL = process.env.PLAYWRIGHT_TEST_BASE_URL!;
 export const HOMEPAGE_ENDPOINT = '/';
 export const WOMEN_COLLECTIONS_ENDPOINT = '/collections/women';
