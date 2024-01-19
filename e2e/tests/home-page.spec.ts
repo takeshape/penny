b5e3d619535ce4f8b44645132877b0b78ffea5a8
PollySt <polina_stadnichenko@pecodesoftware.com>
2024-01-19 17:03:30 +0200

    remove misprint

--- e2e/tests/home-page.spec.ts
+++ e2e/tests/home-page.spec.ts
@@ -3,7 +3,7 @@ import { HOMEPAGE_ENDPOINT } from '../constants';
 
 test.describe('Homepage', () => {
   test('Should load the homepage', async ({ page }) => {
-    await page.goto(HOMEPAGE_ENDPOINT); .
+    await page.goto(HOMEPAGE_ENDPOINT);
     await expect(page.getByRole('link', { name: 'Give a gift' })).toBeVisible();
     await expect(page.getByRole('link', { name: 'Free returns' })).toBeVisible();
     await expect(page.getByRole('link', { name: 'Sign up for our newsletter' })).toBeVisible();
