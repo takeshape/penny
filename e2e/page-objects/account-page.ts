4910dee355adbf3ae8aef2ca5cb55dc93b114f13
PollySt <polina_stadnichenko@pecodesoftware.com>
2024-02-08 13:48:55 +0200

    update locator

--- e2e/page-objects/account-page.ts
+++ e2e/page-objects/account-page.ts
@@ -210,10 +210,7 @@ export class AccountPage {
 
   async verifyUserIsSignedOut() {
     await expect(this.page.getByText('Sign in', { exact: true })).toBeVisible();
-    await expect(this.page.getByTestId('account-icon').getByRole('link')).toHaveAttribute(
-      'href',
-      `/api${SIGN_IN_PAGE_ENDPOINT}`
-    );
+    await expect(this.accountNavIcon().getByRole('link')).toHaveAttribute('href', `/api${SIGN_IN_PAGE_ENDPOINT}`);
   }
 
   async signOut() {
