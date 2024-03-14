c7fe2fd63bec08cfe10ba3fc6410f8a0a66ebbf6
Michael Shick <m@shick.us>
2024-03-13 22:45:36 -0400

    jest config

--- jest.config.js
+++ jest.config.js
@@ -2,16 +2,13 @@
  * @type {import('jest').Config}
  */
 const config = {
-  // add test-utils direct import
   moduleDirectories: ['node_modules', '.jest', 'src'],
-  // add jest-dom and emotion's extra matchers
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
   setupFilesAfterEnv: ['<rootDir>/.jest/jest.setup.js'],
-  // coverage
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
-  // ignore E2E folder
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/e2e/'],
-  // jest 27 introduced 'node' as new default `testEnvironment`
-  // this can be set on a per-file basis: https://jestjs.io/docs/configuration#testenvironment-string
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
