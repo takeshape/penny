784e5724cd0979f67b3087c89ac55b4504fc0dc7
Michael Shick <m@shick.us>
2024-03-18 17:51:40 -0400

    try sharded playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? 'blob' : 'html',
   timeout: 60 * 1000,
   expect: {
     timeout: 20000
