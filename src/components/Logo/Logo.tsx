8840b67011c4f08840f922db4bd06e58790bb233
Michael Shick <m@shick.us>
2024-03-19 11:58:53 -0400

    fix accessibility lighthouse

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -9,7 +9,7 @@ export const Logo = (props: LogoProps) => {
   return (
     <div className="relative flex justify-center items-center">
       <span className="sr-only">{siteName}</span>
-      <NextImage src={siteLogo} alt={siteName} height={100} width={100} className={props.className} />
+      <NextImage src={siteLogo} alt={`Logo for ${siteName}`} height={100} width={100} className={props.className} />
     </div>
   );
 };
