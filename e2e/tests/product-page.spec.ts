4fe725c027017ff72e30fb848b4050752866a804
Michael Shick <m@shick.us>
2024-03-19 10:14:33 -0400

    disable reviews e2e

--- e2e/tests/product-page.spec.ts
+++ e2e/tests/product-page.spec.ts
@@ -72,20 +72,20 @@ test.describe('Write a product review', () => {
     await collectionsPage.selectProduct(PRODUCT_NAME_INSTOCK);
   });
 
-  test('Verify user cannot submit an empty review form', async ({ productPage, page }) => {
+  // TODO Reviews.io acct is currently inactive
+  test.fixme('Verify user cannot submit an empty review form', async ({ productPage, page }) => {
     await productPage.clickOnWriteAReviewBtn();
     await productPage.submitAReviewBtn().click();
     await expect(page.getByText('This field is required')).toHaveCount(2);
   });
 
-  // BUG: https://app.shortcut.com/takeshape/story/12703/product-review-doesn-t-appear-after-submitting-a-review-form
+  // TODO Reviews.io acct is currently inactive
+  // BUG https://app.shortcut.com/takeshape/story/12703/product-review-doesn-t-appear-after-submitting-a-review-form
   test.fixme('Submit a review form', async ({ productPage }) => {
     const message = getTextMessage();
 
     await productPage.clickOnWriteAReviewBtn();
     await productPage.setStarRating(3);
     await productPage.reviewTextarea().fill(message);
-    await productPage.submitAReview();
-    await productPage.verifyLastProductReview({ message, stars: 3 });
   });
 });
